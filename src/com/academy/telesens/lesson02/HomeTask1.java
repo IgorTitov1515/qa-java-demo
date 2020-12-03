cf208c3fed38890abb6a2b720e04031c5379c68f
Titovigor1515@gmail.com <Titovigor1515@gmail.com>
2020-12-03 11:12:43 +0200

    Lesson2 Homework

--- src/com/academy/telesens/lesson02/HomeTask1.java
+++ src/com/academy/telesens/lesson02/HomeTask1.java
@@ -1,13 +1,48 @@
 package com.academy.telesens.lesson02;
 
+import java.util.Scanner;
+
 public class HomeTask1 {
     public static void main(String[] args) {
-        int q = Integer.MAX_VALUE;// макс. десятичное
-        int g  = q + 2;           //минимальное десятичное
-        int r = (q+g)/2;          // среднее значение
 
-        System.out.println(q);
-        System.out.println(g);
-        System.out.println(r);
+        //exercise 1
+
+
+
+//Выбрать из трех чисел максимальное ,минимальное и среднее.
+        System.out.print("Введите первое число: ");
+        Scanner scanner = new Scanner(System.in);
+        Integer x = scanner.nextInt();
+
+        System.out.print("Введите второе число: ");
+        Integer y = scanner.nextInt();
+
+        System.out.print("Введите третье число: ");
+        Integer z = scanner.nextInt();
+
+        System.out.println();
+        if(x > y && x > z )
+            System.out.println("Максимальное число: " + x);
+        if(y > x && y > z )
+            System.out.println("Максимальное число: " + y);
+        if(z > y && z > x )
+            System.out.println("Максимальное число: " + z);
+
+        if(x < y && x < z )
+            System.out.println("Минимальное число: " + x);
+        if(y < x && y < z )
+            System.out.println("Минимальное число: " + y);
+        if(z < y && z < x )
+            System.out.println("Минимальное число: " + z);
+
+        if(x < y && x > z )
+            System.out.println("Cреднее число: " + x);
+        if(y > x && y < z )
+            System.out.println("Cреднее число: " + y);
+        if(z > y && z < x )
+            System.out.println("Cреднее число: " + z);
+
+        //Не работает с double
+
     }
 }
--- src/com/academy/telesens/lesson02/HomeTask2.java
+++ src/com/academy/telesens/lesson02/HomeTask2.java
@@ -4,21 +4,22 @@ import java.util.Scanner;
 
 public class HomeTask2 {
     public static void main(String[] args) {
+
+        //exercise 2
+
         //double weight;//На выводе ошибка
         System.out.println("Ведите ваш земной вес");
         Scanner scanner = new Scanner (System.in);
-        //double weight;//На выводе ошибка
+        double weight;//На выводе ошибка
+         weight = scanner.nextDouble();/*Сканер почемуто округляет до целого числа
         Integer weight = scanner.nextInt();/*Сканер почемуто округляет до целого числа
         вес с запятой не проходит*/
        //double weight;//На выводе ошибка
         double weight2 = weight*0.17;
         //double weight2 = weight*(17/100);//Почему так не вычисляет?
 
-        System.out.println(String.format("Ваш лунный вес %s", weight2));//Вариант1
+        System.out.println(String.format("Ваш лунный вес %s", weight2));
 
-        //  Вариант 2
-          //System.out.println( "Ваш лунный вес:");
-          // System.out.println( weight2);
 
 
 //Задача не решена, ввод возможен только целых чисел
